b9f64dbfceeeac4c4f77e7b67fe1e3b2976ea0f3
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2025-03-19 09:23:56 +0100

    netboot.h: More documentation.

--- lib/netboot.h
+++ lib/netboot.h
@@ -11,6 +11,9 @@
 #define NETBOOT_INET_MAGIC	   "NETB"
 #define NETBOOT_INET_MAGIC_LEN 4
 
+/// @brief Netboot Internet Header
+/// Consists of 4 magic characters, and a set of fields describing the current patch that's being sent (if m_preflight = 0)
+/// @note Can be used to patch ROMs too (if m_implies_rom = 1)
 typedef struct NETBOOT_INET_HEADER
 {
 	char m_nb1; /// magic char 1 'N'
@@ -21,7 +24,7 @@ typedef struct NETBOOT_INET_HEADER
 	char	m_patch_name[255]; /// example: ColdChoco
 	int32_t m_length;		   /// the patch length.
 	char	m_target[255];	   /// the target file.
-	uint8_t ImpliesROM;		   /// does it imply an EEPROM reprogram?
-	uint8_t m_preflight;
+	uint8_t m_implies_rom;		   /// does it imply an EEPROM reprogram?
+	uint8_t m_preflight;		/// Do we just check if this endpoint exists/is valid?
 	uint8_t m_blob[];
 } NETBOOT_INET_HEADER;
